9b9fbb545f61afb7de7380dd1554f7a9e94cbfc3
Doug Nelson <doug.nelson@gmail.com>
2018-05-08 09:58:21 -0500

    Add project files.

---
+++ TimeWeightedAverage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeWeightedAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
